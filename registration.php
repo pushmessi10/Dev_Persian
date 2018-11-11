6513ae0ce3dc15f14bcbb08d7f7733efc7e2025b
pushmessi10 <pushkarmadrap@gmail.com>
2018-11-10 22:24:04 -0600

    php try

---
+++ registration.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+
+         <?php $name= echo $_POST['Name']; ?>
+         <?php $Phno= echo $_POST['Phone_no']; ?>
+         <?php $add= <?php echo $_POST['Address']; ?>
+         <?php $email= <?php echo $_POST['eid']; ?>
+         <?php $DOB= <?php echo $_POST['date']; ?>
+         <?php $Dtype= <?php echo $_POST['dance_type']; ?>
+
+ <h1>REGISTERED SUCCESSFULLY!!!!</h1><br/>
+        <?php echo "document inserted successfully"; ?>
+
+ <h2><u><b><a href="192.168.33.20">BACK</a></u></b></h2>
+
+
+</body>
+</html>
+
